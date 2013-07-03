2bc9b0d341076dd3e98e598b0110b3f3f39e892f
Prathik Raj <prathikraj@ymail.com>
2013-07-03 15:43:00 +0530

    Angular routing to /add and /idea.
    Templates add.html and home.html for angular created.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,46 @@
-function KnoWareController($http, $scope) {
+function navController($scope ) {
 	$scope.user = {name: "Prathik Raj"};
-	var page = 1;	
-	$http.get('server/get-thesis/'+ page).success(function(data) {
-		$scope.ideas = data;
-	});
 
 }
+
+angular.module('knowareapp', []).
+  config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+	when('/ideas', {templateUrl: 'template/home.html',   controller: HomeCntl}).
+	when('/add', {templateUrl: 'template/add.html', controller: AddController}).
+      otherwise({redirectTo: '/ideas'});
+}]);
+
+function AddController($scope, $routeParams, $http) {
+	$scope.message = "<span class=\"label\">Enter the details and click Add</span>";
+	$scope.update = function(idea) {
+		if(typeof idea != "undefined") {
+			var count = 0;
+			var flag = false;
+			for(i in idea) {
+				count++;
+				if(idea[i] == "" || idea[i] == "undefined" || idea[i] == null || typeof idea[i] == "undefined") {
+					flag = true;
+				}
+			}
+			if(count == 4 && !flag) {
+				console.log(idea);
+				$scope.message = "<span class=\"label label-success\">Success! Your idea " + idea.title + " has been posted.</span>";
+			} else {
+				$scope.message = "<span class = \"label label-important\">Fill in all the fields.</span>";
+			}
+			
+		} else {
+			$scope.message = "<span class = \"label label-info\">Fill in all the fields.</span>";
+		}
+	}
+}
+
+function HomeCntl($scope, $routeParams, $http) {
+        var page = 1;
+        $http.get('server/get-thesis/'+ page).success(function(data) {
+                $scope.ideas = data;
+        });
+  $scope.params = $routeParams;
+}
+ 
--- template/index.php
+++ template/index.php
@@ -2,7 +2,7 @@
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 	<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 		<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-			<!--[if gt IE 8]><!--> <html class="no-js" ng-app> <!--<![endif]-->
+			<!--[if gt IE 8]><!--> <html class="no-js" ng-app ="knowareapp"> <!--<![endif]-->
 				<head>
 					<meta charset="utf-8">
 					<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
@@ -22,14 +22,14 @@
 
 					<script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
 				</head>
-				<body ng-controller="KnoWareController">
+				<body>
 					<!--[if lt IE 7]>
 					<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
 					<![endif]-->
 
 					<!-- This code is taken from http://twitter.github.com/bootstrap/examples/hero.html -->
 
-					<div class="navbar navbar-inverse navbar-fixed-top">
+					<div class="navbar navbar-inverse navbar-fixed-top" ng-controller="navController">
 						<div class="navbar-inner">
 							<div class="container">
 								<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
