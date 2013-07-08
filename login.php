7e69af69c53144f76a4e73ebb10c80c1ba67b632
Prathik Raj <prathikraj@ymail.com>
2013-07-08 13:25:07 +0530

    Show message on registration. Design to be done.

--- login.php
+++ login.php
@@ -19,6 +19,8 @@ if( isset($_GET['action']) && $_GET['action'] == 'logout' ) {
 
 if( isset( $_POST['register'] ) && $_POST['register'] == "register" ) {
 	if( $user->register_user( $_POST['username'], $_POST['email'], $_POST['password'], $_POST['fullname'] ) ) {
+		echo "You have been successfully registerd.";
+		echo "<br />Page under construction."
 		header("Location: ./");
 	}
 }
