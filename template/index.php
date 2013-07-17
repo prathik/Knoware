b78d6fd3c185ff902ad862b5edd7c009e72a9764
Prathik Raj <prathikraj@ymail.com>
2013-07-17 17:59:22 +0530

    Links update.

--- template/index.php
+++ template/index.php
@@ -39,7 +39,7 @@
 									<span class="icon-bar"></span>
 									<span class="icon-bar"></span>
 								</a>
-								<a class="brand" href="#">KnoWare</a>
+								<a class="brand" href="./">KnoWare</a>
 								<div class="nav-collapse collapse">
 									<ul class="nav">
 										<li><a href="#/about">About</a></li>
